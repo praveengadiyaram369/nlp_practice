2fd2df509d600522e0948eacabddff59b2bfeace
Sri Sai Praveen Gadiyaram <srisaipraveengadiyaram@Sris-Air.fritz.box>
2021-02-26 10:44:31 +0100

    adding changes for lemmatizer

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ This repository holds all the work related to Natural Language Processing work o
 
 credits: 
 1) https://youtu.be/8rXD5-xhemo
-2) nltk - 
+2) nltk - https://youtube.com/playlist?list=PLZoTAELRMXVMdJ5sqbCK2LiM0HhQVWNzm
 3) spaCy - 
+4) Regular expressions - https://www.youtube.com/watch?v=K8L6KVGG-7o
--- practice_exercises/nltk_tokenization.py
+++ practice_exercises/nltk_tokenization.py
@@ -1,6 +1,6 @@
 import nltk
 
-corpus = 'Natural language processing (NLP) is a subfield of linguistics, computer science, and artificial intelligence concerned with the interactions between computers and human language, in particular how to program computers to process and analyze large amounts of natural language data. The result is a computer capable of "understanding" the contents of documents, including the contextual nuances of the language within them. The technology can then accurately extract information and insights contained in the documents as well as categorize and organize the documents themselves.'
+corpus = '''Natural language processing (NLP) is a subfield of linguistics, computer science, and artificial intelligence concerned with the interactions between computers and human language, in particular how to program computers to process and analyze large amounts of natural language data. The result is a computer capable of "understanding" the contents of documents, including the contextual nuances of the language within them. The technology can then accurately extract information and insights contained in the documents as well as categorize and organize the documents themselves.'''
 
 documents = nltk.sent_tokenize(corpus)
 print(documents)
